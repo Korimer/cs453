2ee7e0ff17be0117797195f73c39fcff87b910c8
Korimer <thekclov@gmail.com>
2026-01-30 19:32:17 -0700

    first entry or smth

---
+++ hw1/Parse.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parse {
+
+  public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    ArrayList<String> lines = new ArrayList<>();
+    
+  }
+}
+
+enum Operator {
+  Plus("+"),
+  Minus("-"),
+  Mult("*"),
+  Div("/")
+  ;
+  public final String symbol;
+  Operator(String symbol) {
+    this.symbol = symbol;
+  }
+}
