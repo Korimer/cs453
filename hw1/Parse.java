7c2f6f22ad237efb20c1adbff2793791b667856e
Korimer <thekclov@gmail.com>
2026-02-22 09:39:06 -0700

    sync (deadline!)

--- hw1/Parse.java
+++ hw1/Parse.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,14 +13,146 @@ public class Parse {
   }
 }
 
-enum Operator {
-  Plus("+"),
-  Minus("-"),
-  Mult("*"),
-  Div("/")
+class Tokenizer {
+
+  public static ExprToken tokenize(ArrayDeque<ClassifiedString> classified) {
+
+    return null;
+  }
+
+  public static ArrayDeque<ClassifiedString> classifyString(String s) throws SyntaxParsingException {
+    ArrayDeque<ClassifiedString> classified = new ArrayDeque<>();
+    String[] preparsed_lines = preparse(s);
+
+    for (int i=0; i<preparsed_lines.length; i++) {
+      String line = preparsed_lines[i];
+      for (int j=0; j<line.length(); j++) {
+        ClassifiedString result = identify(s.substring(j,j+1));
+        if (result == null)
+          throw new SyntaxParsingException(i+1);
+        classified.add(result);
+      }
+    }
+
+    classified.add(new ClassifiedString("", StringType.EOL));
+
+    return classified;
+  }
+  private static String[] preparse(String s) throws SyntaxParsingException {
+    String[] lines = s.split("\\n");
+    for (int i=0; i<lines.length; i++) {
+      String line = lines[i];
+      if (line.contains(">") || line.contains("<"))
+        throw new SyntaxParsingException(i+1);
+      lines[i] = line
+        .replace("++",">")
+        .replace("--","<");
+    }
+    
+    return lines;
+  }
+
+  private static ClassifiedString identify(String s) {
+    for (StringType stype : StringType.values()) {
+      if (stype.chars.contains(s))
+        return new ClassifiedString(s, stype);
+    }
+    return null;
+  }
+}
+
+enum TokenType {
+  Epxr,
+  BaseExpr, RecExpr,
+  MonoExpr,
+  PostfixExpr, RecPostfix,
+  Literal,
+  Lvalue,
+  Num,
+  Incrop,
+  Binop,
+}
+
+public class ExprToken extends Token {
+  
+}
+
+
+abstract class Token {
+  public final TokenOrString inner;
+  protected final TokenType type;
+
+  public Token(Token t, TokenType type) {
+    this(new TokenOrString(t), type);
+  }
+  public Token(String s, TokenType type) {
+    this(new TokenOrString(s), type);
+  }
+
+  private Token(TokenOrString ts, TokenType type) {
+    this.inner = ts;
+    this.type = type;
+  }
+
+  public String getContent() {
+    return inner.getContent();
+  }
+}
+
+class TokenOrString {
+  private boolean isToken;
+  private Token token = null;
+  private String string = null;
+  
+  public TokenOrString(Token t) {
+    this.isToken = true;
+    this.token = t;
+  }
+  public TokenOrString(String s) {
+    this.isToken = false;
+    this.string = s;
+  }
+
+  public String getContent() {
+    if (this.isToken) {
+      return this.token.getContent();
+    }
+    else {
+      return this.string;
+    }
+  }
+}
+
+enum StringType {
+  Num("0123456789"),
+  Incrop("<>"),
+  Binop("+-"),
+  OpnParen("("),
+  ClsParen(")"),
+  Ref("$"),
+  EOL(""),
   ;
-  public final String symbol;
-  Operator(String symbol) {
-    this.symbol = symbol;
+  public final String chars;
+  private StringType(String chars) {
+    this.chars = chars;
+  }
+}
+
+class ClassifiedString {
+  public final String inner;
+  public final StringType type;
+  public ClassifiedString(String content, StringType s) {
+    this.inner = content;
+    this.type = s;
+  }
+}
+
+
+class SyntaxParsingException extends Exception {
+  public SyntaxParsingException(String m) {
+    super(m);
+  }
+  public SyntaxParsingException(int line) {
+    super("Parse error in line " + line);
   }
 }
